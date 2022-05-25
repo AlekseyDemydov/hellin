669a41e71e53b1e046050df0689a7da025779b78
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2022-05-25 18:44:26 +0300

    update js

--- src/slider.js
+++ src/slider.js
@@ -1,15 +1,36 @@
-$('.slider-for').slick({
+$('.slider-top-js').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
   fade: true,
-  asNavFor: '.slider-nav'
+  asNavFor: '.slider-botton.js'
 });
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true
-});
+$('.slider-botton-js').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-top-js',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    responsive: [{
+      
+        breakpoint: 1366,
+        settings: {
+            slidesToShow: 3,
+            infinite: true
+        }
+      
+    }, {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 7,
+            slidesToScroll: 1,
+        }
+    }, {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+        }
+    }]
+})
